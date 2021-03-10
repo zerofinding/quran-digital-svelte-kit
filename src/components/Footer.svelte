6b44b131c78d0945b31c9ed602db8234ddcef13b
Zero F <71748685+zerofinding@users.noreply.github.com>
2021-03-10 14:16:48 +0700

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <div style="padding-top: 50px">
   <p class="text-center text-muted">
-    al-quran digital by <a href="https://github.com/zerofinding">Finding Nemo</a> made
+    al-quran digital by <a href="https://github.com/zerofinding/quran-digital-svelte-kit">Finding Nemo</a> made
     with <span style="color: #ff3e00">Svelte</span> | api
     <a href="https://api.quran.sutanlab.id/">sutanlab</a>
   </p>
